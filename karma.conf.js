4f73aa7e0fc07d75a7aaafb97f6890f9a6263947
cooper <scttcper@gmail.com>
2015-05-08 14:52:25 -0700

    fix dist

--- karma.conf.js
+++ karma.conf.js
@@ -33,7 +33,7 @@ module.exports = function(config) {
 
     ngHtml2JsPreprocessor: {
       stripPrefix: 'src/',
-      moduleName: 'honbotClient'
+      moduleName: 'client'
     },
 
     browsers : ['PhantomJS'],
